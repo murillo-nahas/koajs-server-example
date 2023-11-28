bfcaaa61845359d95393dfe4633370aec19bc764
Murillo Nahás <murillonahas2003@gmail.com>
2023-11-27 23:03:06 -0300

    chore: using bodyparser and json

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import Koa from "koa";
+import bodyParser from "koa-bodyparser";
+import json from "koa-json";
+
 import { router } from "./routes";
 
 const app = new Koa();
 
 const port = 3000;
 
+app.use(bodyParser());
+app.use(json());
 app.use(router.routes());
 
 app.listen(port, () => {
