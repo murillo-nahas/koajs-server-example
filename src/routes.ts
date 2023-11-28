f86696abe2ecbc8e3d0ffd6810780a951314111d
Murillo Nahás <murillonahas2003@gmail.com>
2023-11-27 23:03:16 -0300

    feat: adding routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,15 @@
 import Router from "koa-router";
 
+import { listProducts } from "./app/useCases/listProducts";
+import { listProductById } from "./app/useCases/listProductById";
+import { createProduct } from "./app/useCases/createProduct";
+import { updateProduct } from "./app/useCases/updateProduct";
+import { deleteProduct } from "./app/useCases/deleteProduct";
+
 export const router = new Router();
 
-router.get("/product", (ctx: any, next: any) => {
-  ctx.body = "Hello World!";
-});
+router.get("/product", listProducts);
+router.get("/product/:id", listProductById);
+router.post("/product", createProduct);
+router.put("/product/:id", updateProduct);
+router.delete("/product/:id", deleteProduct);
